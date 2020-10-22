deab61f0f797fcc9b69065481bbb4d137abd381c
mustafakaplan <mstfakapln@gmail.com>
2020-10-22 23:53:44 +0300

    backend project created

---
+++ tradeify_ws/src/main/java/com/tradeify/tradeify_ws/TradeifyWsApplication.java
@@ -0,0 +1,13 @@
+package com.tradeify.tradeify_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeifyWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeifyWsApplication.class, args);
+	}
+
+}
---
+++ tradeify_ws/src/test/java/com/tradeify/tradeify_ws/TradeifyWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradeify.tradeify_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeifyWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
