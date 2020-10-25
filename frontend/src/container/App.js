bf28566352f69785bf2714062ee23b5f57ff26c8
mustafakaplan <mstfakapln@gmail.com>
2020-10-25 23:45:11 +0300

    react router dom

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import HomePage from "../pages/HomePage";
+import UserSignupPage from "../pages/UserSignupPage";
+import LoginPage from "../pages/LoginPage";
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 
-function App() {
+const App = () => {
+  const { isLoggedIn } = useSelector((store) => ({
+    isLoggedIn: store.isLoggedIn,
+  }));
+
   return (
     <div>
-      E-Commerce
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {!isLoggedIn && <Route path="/login" component={LoginPage} />}
+          {!isLoggedIn && <Route path="/signup" component={UserSignupPage} />}
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,17 +4,15 @@ import "./index.css";
 import App from "./container/App";
 import "./bootstrap-override.scss";
 import * as serviceWorker from "./serviceWorker";
-import UserSignupPage from "./pages/UserSignupPage";
 import "./i18n";
 import { Provider } from "react-redux";
 import configureStore from "./redux/configureStore";
-import LoginPage from "./pages/LoginPage";
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <LoginPage />
+    <App />
   </Provider>,
   document.getElementById("root")
 );
